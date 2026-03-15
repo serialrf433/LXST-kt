8b21a5edd1287f3cc63a323ca45996d09316ba91
serialrf433 <iuwfg433@protonmail.com>
2026-03-15 21:46:41 +0000

    Update build.gradle.kts - agp9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file for LXST-kt standalone library
 plugins {
-    id("com.android.library") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("com.android.library") version "9.10.0" apply false
+    // id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
